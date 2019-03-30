36832d45451196f7771a4a2b40a4fd2a15426dda
Denis Kokorev <nvsbleat@gmail.com>
2019-03-30 22:32:22 +0300

    Рабочее решение
    Есть три вида фигур:
    Треугольник, Прямоугольник, Круг. Они выводятся на экран. Программа сотрирует массив фигур по возрастанию площади.

--- Figure.h
+++ Figure.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <windows.h>
+#include <math.h>
 #define PI 3.14159265359
 
+using namespace std;
 
+//Абстрактный класс - интерфейс
 class Figure
 {
 private:
 	COLORREF color;
 protected:
-	int x, y; // базовая точка
+	int center_x, center_y; // базовая точка
 public:
 	
 	void set_color(COLORREF c)
@@ -21,8 +24,8 @@ public:
 	}
 	void set_position(int x, int y)
 	{
-		this->x = x;
-		this->y = y;
+		this->center_x = x;
+		this->center_y = y;
 	}
 	
 	virtual double area() = 0;
@@ -31,26 +34,26 @@ public:
 
 };
 
-class Circle : public Figure
+class MyCircle : public Figure
 {
 private:
-	int radius;
+	int diameter;
 public:
-	Circle(COLORREF c, int x, int y, int r) 
+	MyCircle(COLORREF c, int x, int y, int d) 
 	{
 		set_color(c);
-		this->x = x;
-		this->y = y;
-		this->radius = r;
+		this->center_x = x;
+		this->center_y = y;
+		this->diameter = d;
 	}
-	~Circle()
+	~MyCircle()
 	{
 	
 	}
 
 	virtual double area()
 	{
-		double rr = (double)(radius*radius);
+		double rr = (double)(diameter*diameter)/4.0;
 		return PI*rr;
 	}
 	virtual void draw()
@@ -70,7 +73,103 @@ public:
 		HBRUSH brush = CreateSolidBrush(fcolor);
 		SelectObject(dc, pen);
 		SelectObject(dc, brush);
-		Ellipse(dc, x - radius, y + radius, x + radius, y - radius);
+		Ellipse(dc, center_x - diameter/2, center_y + diameter/2, center_x + diameter/2, center_y - diameter/2);
+		ReleaseDC(console, dc);
+	}
+
+
+};
+
+
+
+class MyRectangle : public Figure
+{
+private:
+	int height;
+	int width;
+
+public:
+	MyRectangle(COLORREF c, int x, int y, int h, int w)
+	{
+		set_color(c);
+		this->center_x = x;
+		this->center_y = y;
+		this->height = h;
+		this->width = w;
+	}
+	~MyRectangle()
+	{
+
+	}
+
+	virtual double area()
+	{
+		
+		return height*width;
+	}
+	virtual void draw()
+	{
+		HWND console = FindWindow("ConsoleWindowClass", NULL);
+		HDC dc = GetDC(console);
+		COLORREF bcolor = get_color();
+
+
+		HPEN pen = CreatePen(PS_SOLID, 2, bcolor);
+		COLORREF fcolor = RGB(0, 0, 0);
+
+
+		HBRUSH brush = CreateSolidBrush(fcolor);
+		SelectObject(dc, pen);
+		SelectObject(dc, brush);
+		Rectangle(dc, center_x - height / 2, center_y + width / 2, center_x + height / 2, center_y - width / 2);
+		ReleaseDC(console, dc);
+	}
+
+
+};
+
+
+class MyTriangle : public Figure
+{
+private:
+	int side;
+
+public:
+	MyTriangle(COLORREF c, int x, int y, int s)
+	{
+		set_color(c);
+		this->center_x = x;
+		this->center_y = y;
+		this->side = s;
+	
+	}
+	~MyTriangle()
+	{
+
+	}
+
+	virtual double area()
+	{
+
+		return sqrt(3.0)*(side*side)/4.0;
+	}
+	virtual void draw()
+	{
+		HWND console = FindWindow("ConsoleWindowClass", NULL);
+		HDC dc = GetDC(console);
+		COLORREF bcolor = get_color();
+
+
+		HPEN pen = CreatePen(PS_SOLID, 2, bcolor);
+		COLORREF fcolor = RGB(0, 0, 0);
+
+
+		HBRUSH brush = CreateSolidBrush(fcolor);
+		SelectObject(dc, pen);
+		SelectObject(dc, brush);
+		POINT vertices[] = { { center_x - side/2, center_y - side / 2 },{ center_x, center_y + side / 2 },{ center_x + side / 2, center_y - side / 2 } };
+		Polygon(dc, vertices, sizeof(vertices) / sizeof(vertices[0]));
+		
 		ReleaseDC(console, dc);
 	}
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include "Figure.h"
+#include <vector>
+#include <algorithm>
+#include <memory>
 
 int main(void)
 {
-	Circle circle(RGB(255,0,0),50,50,10);
+	int SIZE = 100; // выберем одно значение на все фигуры
+	int x = 0, y = 0;
+	std::vector<std::unique_ptr<Figure>> vec;
+	
 
-	circle.draw();
+	vec.push_back(std::unique_ptr<Figure>(new MyCircle(RGB(255, 0, 0), x, y, SIZE))); //Круг
+	vec.push_back(std::unique_ptr<Figure>(new MyRectangle(RGB(0, 255, 0), x, y, SIZE,SIZE))); //Квадрат
+	vec.push_back(std::unique_ptr<Figure>(new MyTriangle(RGB(0, 0, 255), x, y, SIZE))); //Равносторонний треугольник
+	
+	
+	auto func = [](const auto & a, const auto & b) -> bool
+	{
+		return a->area() > b->area();
+	};
+	//Сортируем фигуры по площади
+	std::sort(vec.begin(), vec.end(),func);
+	
+	x = 150;
+	y = 400;
+	int step = SIZE + 50;
+
+	//Выводим на экран
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		vec[i]->set_position(x + i * step, y);
+		vec[i]->draw();
+	}
+	/**/
 
 	system("pause");
 	return 0;
